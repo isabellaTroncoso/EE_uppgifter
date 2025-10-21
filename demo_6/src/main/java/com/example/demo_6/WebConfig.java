e974e74e6bbcd9e222eb2a4b49001f2ffa7da88b
isabellaTroncoso <isabella.troncoso@stud.sti.se>
2025-10-21 14:24:30 +0200

    All the assignments from lecture six (spring security)

---
+++ demo_6/src/main/java/com/example/demo_6/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.demo_6;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/admin").setViewName("admin");
+    }
+}
